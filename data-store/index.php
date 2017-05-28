d255aff01e0fc2473f3a28e9b34ccda783be87bf
Muhammad Faruq Nuruddinsyah <faruq.nuruddinsyah@gmail.com>
2017-05-29 04:25:32 +0700

    Update index.php

--- data-store/index.php
+++ data-store/index.php
@@ -6,20 +6,10 @@
  Oleh: Muhammad Faruq Nuruddinsyah
  Copyright 2017. All Rights Reserved.
 ------------------------------------------------------------
- Request Handler
+ Data Store
 ------------------------------------------------------------
 */
 
-define('BASE_PATH', 'gui/'); include(BASE_PATH . 'luxi.php');
-include('core/core.php');
-
-$service = $_POST['service'];
-$function = $_POST['function'];
-
-$file_json = file_get_contents("services/$service/$service.json");
-$json = json_decode($file_json);
-$json_function = get_json_function($json->service, $function);
-
-include("services/$service/$function.php");
+// Empty file
 
 ?>
